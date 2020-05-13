aa1d213b287e946c51076ea5b390e26695b018ba
Marc Serra <mserrahidalgo@gmail.com>
2020-05-13 19:07:38 +0200

    change to delete

--- services/projects.js
+++ services/projects.js
@@ -1,31 +1,29 @@
 const UserProject = require('../models/UserProject')
-const User = require('../models/User')
 const ApiError = require('../lib/errors')
 
 async function getAll (currentUser) {
-  return await UserProject.find({ user: currentUser._id }) 
+  return await UserProject.find({ user: currentUser._id })
 }
 
 async function deleteOne (projectId, currentUser) {
-  if(!userProject.user === currentUser._id) throw new ApiError(401, 'Its not your project')
-  const userProject = await UserProject.findByIdAndDelete(projectId)
-  return userProject
+  const userProject = await UserProject.findById(projectId)
+  if (!userProject.user === currentUser._id) throw new ApiError(401, 'Its not your project')
+  return await UserProject.findByIdAndDelete(projectId)
 }
 
 async function getOne (projectId, currentUser) {
   const userProject = await UserProject.findById(projectId)
-  if(!userProject.user === currentUser._id) throw new ApiError(401, 'Its not your project')
+  if (!userProject.user === currentUser._id) throw new ApiError(401, 'Its not your project')
   return userProject
 }
 
 async function createProject (currentUser, name, style) {
   return await UserProject.create({ name, style, user: currentUser._id })
-  
 }
 
 async function updateProject (projectId, componentsConfiguration, currentUser) {
   const userProject = await UserProject.findById(projectId)
-  if(!userProject.user === currentUser._id) throw new ApiError(401, 'Its not your project')
+  if (!userProject.user === currentUser._id) throw new ApiError(401, 'Its not your project')
   userProject.componentsConfiguration = componentsConfiguration
   // const updatedProject = await UserProject.findByIdAndUpdate(projectId, { componentsConfiguration }, { new: true })
   return await userProject.save()
